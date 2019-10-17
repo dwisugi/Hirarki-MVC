803da3e082e81a659702d08d2af88c7284a54788
dwisugi <dwisugianto17@gmial.com>
2019-10-17 20:21:15 +0700

    first

---
+++ application/modules/admin/controllers/Admin.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends MX_Controller {
+	// require_once APPPATH.'libraries/JWT.php';
+	// use \Firebase\JWT\JWT;
+	public function index()
+	{
+		echo 'Selamat datang di HMVC saya';
+	}
+}
